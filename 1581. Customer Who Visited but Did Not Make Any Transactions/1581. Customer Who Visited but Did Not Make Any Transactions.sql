b3d1bb42fda1a6a1735f9de8dcc5598b4a1b0371
datawithpradeeptelugu <datawithpradeeptelugu@gmail.com>
2025-11-06 20:14:31 +0530

    Add files via upload

---
+++ 1581. Customer Who Visited but Did Not Make Any Transactions/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Visits (
+    visit_id INT PRIMARY KEY,
+    customer_id INT
+);
+
+CREATE TABLE Transactions (
+    transaction_id INT PRIMARY KEY,
+    visit_id INT,
+    amount INT
+);
+
+
+INSERT INTO Visits (visit_id, customer_id) VALUES
+(1, 23),
+(2, 9),
+(4, 30),
+(5, 54),
+(6, 96),
+(7, 54),
+(8, 54);
+
+INSERT INTO Transactions (transaction_id, visit_id, amount) VALUES
+(2, 5, 310),
+(3, 5, 300),
+(9, 5, 200),
+(12, 1, 910),
+(13, 2, 970);
+
+SELECT 
+    customer_id,
+    COUNT(*) count_no_trans
+
+FROM Visits V
+LEFT JOIN Transactions T
+ON V.visit_id= T.visit_id
+WHERE T.visit_id IS NULL
+GROUP BY customer_id
+
