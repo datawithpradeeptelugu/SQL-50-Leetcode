0e713f674960bee1e77220f8cc610e77304e00c6
datawithpradeeptelugu <datawithpradeeptelugu@gmail.com>
2025-11-22 08:15:30 +0530

    Delete 1075. Project Employees I directory

--- 1075. Project Employees I/1075. Project Employees I.sql
+++
@@ -1,9 +0,0 @@
-/* Write your T-SQL query statement below */
-Select project_id,
---sum(E.experience_years),
---count(E.employee_id),
-Cast(sum(E.experience_years)*1.0/count(E.employee_id) as decimal(10,2)) as average_years
-FROM Project P
-LEFT JOIN Employee E
-ON P.employee_id=E.employee_id
-group by p.project_id
